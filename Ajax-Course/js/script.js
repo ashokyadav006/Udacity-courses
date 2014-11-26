4cd1d654586cf67ae1b1b7d8dfd6bcbdb7fc8223
ashok yadav <ashokyadav006@gmail.com>
2014-11-27 00:51:20 +0530

    Add ajax request

--- Ajax-Course/js/script.js
+++ Ajax-Course/js/script.js
@@ -18,6 +18,45 @@ function loadData() {
     var url = "https://maps.googleapis.com/maps/api/streetview?size=600x400&location="+address;
     $body.append('<img class="bgimg" src='+url+'>');
 
+    //load new york times article
+    var url = "http://api.nytimes.com/svc/search/v2/articlesearch.json?q="+address+"&api-key=429b9d3bdd92a40cdb4f15268d2a9bab:1:70213918";
+
+    $.getJSON(url)
+        .done(function (data) {
+            var articles = data.response.docs;
+            var article;
+            var listElement = '<li class="article">'
+            for (var i=0; i < articles.length; i++) {
+                article = articles[i];
+                listElement += '<a href='+article.web_url+'><h3>'+article.headline.main+'</h3></a>';
+                listElement += '<p>'+article.lead_paragraph+'</p>';                
+                listElement += '</li>'
+                $nytElem.append(listElement);
+            }
+        })
+        .fail(function (data) {
+            $("#nytimes-header").text("New York Times articles could not be loaded.")
+        });
+
+
+    //Load wiki links
+    var wikiRequestTimeout = setTimeout(function () {
+        $wikiElem.text("Not able to load wikipedia links");
+    }, 8000);
+
+    var wikiUrl = "http://en.wikipedia.org/w/api.php?action=opensearch&search="+address+"&format=json&callback=wikiCallback";
+    $.ajax({
+        url: wikiUrl,
+        dataType: "jsonp"
+    })
+    .done(function (response) {
+        var articleList = response[1];
+
+        for (var i=0; i < articleList.length; i++) {
+            $wikiElem.append('<li><a href="http://en.wikipedia.org/wiki/"'+articleList[i]+'>'+articleList[i]+'</a></li>')
+        }
+        clearTimeout(wikiRequestTimeout);
+    });
 
     return false;
 };
